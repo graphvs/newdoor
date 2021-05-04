643909071fa6e615b0bcfd086206e0d3b643bb62
Kali <pennymole365@gmail.com>
2021-05-04 16:14:47 -0400

    second

--- newdoor.py
+++ newdoor.py
@@ -1,17 +1,26 @@
 #!/usr/bin/env python
 import sys
+import requests
+
 
 #arguments should be things like cookies or IDs to use in a request
 #last arguments should be a text file with the urls
-ID= sys.argv[0]
-cookie=sys.argv[1]
 
+
+if len(sys.argv) > 3:
+	ID= sys.argv[1]
+	cookie=sys.argv[2]
 	
-#read the text file
-f = open(sys.argv[2],"r")
-    contents = f.read()
-    f.close()
-    print(ID,cookie,contents)
+	#read the text file
+	f = open(sys.argv[3],"r")
+	line = f.readline().strip("\n")
+	for line in f:
+		response = requests.get('https://api.github.com')
+		params={'id': ID,'cookie',cookie}
+		print(line,response.status_code,ID,cookie)
+		
+	f.close()
+	    
     
     	
     	
